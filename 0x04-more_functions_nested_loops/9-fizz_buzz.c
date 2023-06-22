da6387e2ef12055ed2266463115a36e756316239
mehdileDeuxieme <Mehdibusinessacc@gmail.com>
2023-06-22 18:58:42 +0100

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,20 +9,27 @@
 int main(void)
 {
 int i;
-for (i = 1 ; i <= 100 ; i++)
+for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0 && i % 5 == 0)
+if (i % 3 == 0 && i % 5 != 0)
 {
-printf("FizzBuzz ");
+printf(" Fizz");
+} else if (i % 5 == 0 && i % 3 != 0)
+{
+printf(" Buzz");
+} else if (i % 3 == 0 && i % 5 == 0)
+{
+printf(" FizzBuzz");
 }
-else if (i % 3 == 0)
+else if (i == 1)
 {
-printf("Fizz ");
+printf("%d", i);
 }
-else if (i % 5 == 0)
+else
 {
-printf("Buzz ");
+printf(" %d", i);
 }
 }
 printf("\n");
+return (0);
 }
